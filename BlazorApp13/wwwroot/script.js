f43dfb674c706fb1d5c91a604bcafb09621b4a8f
darren <darreno@onestepsystems.com>
2020-09-16 13:26:03 -0700

    first commit

---
+++ BlazorApp13/wwwroot/script.js
@@ -0,0 +1,26 @@
+﻿
+var MyJs = {
+
+    GetLocation: async function() {
+        var position = await MyJs.GetCoordinates();
+        if (position) {
+            var result = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+            return result;
+        } else {
+            return null;
+        }
+    },
+
+    GetCoordinates: async function() {
+        let promise = new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+        let result = await promise;
+        console.log('MyJs.GetCoordinates done');
+        return result; // "done!"
+    },
+
+    HandleError: function(error) {
+        console.log('Error: ', error); alert(error);
+    }
+}
